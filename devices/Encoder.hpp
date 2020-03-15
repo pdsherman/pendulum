b9f4cc018a7f700b0f1f0490acf5be05acda52f4
pdsherman <pdsherman201@gmail.com>
2020-03-15 08:40:22 -0700

    Add test method for analog input

--- devices/Encoder.hpp
+++ devices/Encoder.hpp
@@ -10,27 +10,45 @@
 #include <stdint.h>
 #include <string>
 
+/// Interface for an encoder reading device.
 class Encoder {
 public:
+  /// Constructor
+  /// @param [in] i2c_dev Name for i2c peripheral to use
+  /// @param [in] addr Address for device reading the encoder
+  Encoder(const std::string &i2c_dev, const uint8_t addr);
 
-    Encoder(const std::string &i2c_dev, const uint8_t addr);
+  /// Destructor
+  ~Encoder(void);
 
-    ~Encoder(void);
+  /// Open connection to peripherial
+  /// @return True if connection successful
+  bool connect(void);
 
-    bool connect(void);
+  /// Read the position of the encoder
+  /// @return Encoder position in counts
+  int32_t position(void);
 
-    int32_t position(void);
+  /// Set the current positino of the encoder to 0
+  void zero_position(void);
 
-    void zero_position(void);
+  void all_data(int32_t &pos, int16_t &sldr);
 
 private:
 
+  /// Close the connection to device
+  /// @return True if connection was open and successfully closed
   bool close_port(void);
 
+  /// Is the connection currently open
   bool _is_open;
 
+  /// Name for i2c peripheral
   std::string _dev_name;
-  uint8_t _slave_addr;
-  int _file_des;
 
+  /// Slave address for device
+  uint8_t _slave_addr;
+
+  /// File descriptor for connection
+  int _file_des;
 };
