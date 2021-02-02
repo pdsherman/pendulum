a2cda839ac9cbf36c2300f3d653d8e33a208135b
pdsherman <pdsherman201@gmail.com>
2021-02-01 22:05:50 -0800

    Serial Test with Copley

--- devices/motor/src/main.cpp
+++ devices/motor/src/main.cpp
@@ -9,8 +9,33 @@
 int main(int argc, char* argv[])
 {
 
-  std::string port = "/dev/serial0";
+  std::string port_addr = "/dev/serial0";
 
+  SerialPort port();
+
+  port.Open(port_addr);
+
+  if(!port.IsOpen()) {
+    std::cout << "Error: Unable to Open" << std::endl;
+    return -1;
+  }
+
+  port.SetBaudRate(BaudRate::BAUD_9600);
+  port.SetCharacterSize(CharacterSize::CHAR_SIZE_8);
+  port.SetFlowControl(FlowControl::FLOW_CONTROL_NONE);
+  port.SetParity(Parity::PARITY_NONE);
+  port.SetStopBits(StopBits::STOP_BITS_1);
+
+
+  std::string msg = "g f0x41";
+  msg += '\r';
+
+  port.Write(msg);
+
+  std::string rx_msg = "";
+  port.ReadLine(rx_msg, '\r', 25);
+
+  std::cout << "Received: " << rx_msg << std::endl;
 
   return 0;
 }
