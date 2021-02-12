99ba6064479f2f9ceddc2ad5c144067735408b1e
pdsherman <pdsherman201@gmail.com>
2021-02-11 21:51:04 -0800

    Testing motor

--- devices/motor/src/main.cpp
+++ devices/motor/src/main.cpp
@@ -1,11 +1,6 @@
 
 
-#include <external_libs/copley/CopleyCommandBuilderAscii.hpp>
-#include <external_libs/copley/CopleyCommandBuilderBinary.hpp>
-#include <external_libs/copley/CopleyResponseBinary.hpp>
-#include <external_libs/copley/CopleyResponseAscii.hpp>
-#include <external_libs/copley/CopleyBus.hpp>
-#include <external_libs/copley/CopleyParameter.hpp>
+#include <external_libs/copley/Motor.hpp>
 
 #include <string>
 #include <iostream>
@@ -15,18 +10,8 @@ using namespace LibSerial;
 int main(int argc, char* argv[])
 {
 
-  std::string serial_port_addr = "/dev/serial0";
-  CopleyBus bus;
-  if(bus.connect(serial_port_addr)) {
-    CopleyCommandBuilderBinary cmd_builder(0);
-    CopleyCommand cmd = cmd_builder.build_get_command(CopleyParameter::kMotorModel,
-      CopleyParameter::kMotorModel.location);
-
-    std::unique_pre<CopleyResponse> resp = bus.write_command(cmd);
-    std::cout << "Response: " << resp->to_string() << std::endl;
-
-  }
-
+  Motor mtr;
+  mtr.test_method();
 
   return 0;
 }
